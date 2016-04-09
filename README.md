7ee988db321b20ffe8b19f0f5f152b5a29d65569
James Bithell <james@bithell.com>
2016-04-09 20:06:49 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Bundler
 System to Bundle PHP/Twig Projects into html for use in PhoneGap and Element
+##IN DEVELOPMENT
+##Usage
+<ol>
+	<li>Clone entire repo into a directory you use for the development area of your site (<i>ie beta.yourdomain.com</i>)</li>
+	<li>Place all your required files, e.g. bootstrap.css, images, etc. in weblibs/</li>
+	<li>Place all your twig files in twig/
+		<ul>
+			<li>For Javascript etc. or templates that you need for twig place them in the twig file and reference them within your twig.</li>
+			<li>All twig paths are relative to twig/</li>
+		</ul>
+	</li>
+	<li>For each page add it to the $PAGES variable in index.php, with its "TWIG" set to the path (from within twig/) of that file, and the key to the page url (ie for the contactus page you may set it to "contactus" - Only use paths one level deep - don't use "contact/us")</li>
+	<li>For a live view and testing - access your directory (ie beta.yourdomain.com) /PAGENAME for each page - the index file is returned if no page name is passed.</li>
+	<li>For each release call php index.php "VERSION NUMBER (ie 0.1.1)" from the command line - your release will be saved in releaes/VERSION NUMBER/ - and upload it to your site</li>
+</ol>
--- index.php
+++ index.php
@@ -12,7 +12,7 @@ require_once __DIR__ . '/composer/vendor/autoload.php';
  * @param	   string   $permissions New folder creation permissions
  * @return	  bool	 Returns true on success, false on failure
  */
-function xcopy($source, $dest, $permissions = 0755)
+function xcopy($source, $dest, $permissions = 0777)
 {
 	// Check for symlinks
 	if (is_link($source)) {
@@ -45,21 +45,56 @@ function xcopy($source, $dest, $permissions = 0755)
 	$dir->close();
 	return true;
 }
-
-
-//Config of Pages
-$PAGES = [
-	'INDEX' => ["TWIG" => "index.twig"],
-];
+//Load Twig
+$GLOBALS['TWIGLOADER'] = new Twig_Loader_Filesystem(__DIR__ . '/twig/');
+$GLOBALS['TWIG'] = new Twig_Environment($GLOBALS['TWIGLOADER'], array(
+	'debug' => true
+));
+$GLOBALS['TWIG']->addExtension(new Twig_Extension_Debug());
+function twigrender($file) {
+	global $GLOBALS;
+	return $GLOBALS['TWIG']->render($file, $GLOBALS);
+}
+require_once __DIR__ . '/pages.php';
 
 
 if (php_sapi_name() == "cli") { //Command line mode, this will bundle
 	echo "\n\n\t\t\t\tJAMES BITHELL\n\t\t\t\tTWIG BUNDLER\n\n";
 	if (!isset($argv[1])) die("You must set a version number \n\nEXAMPLE: php index.php " . '"1.0"' . "\n\n\n");
+	$newreleasepath = __DIR__ . '/releases/' . $argv[1] . '/';
+	if (file_exists($newreleasepath)) die("Release already exisits - Make a new one!!\n\n");
+	mkdir($newreleasepath,0777,true);
+
+	xcopy(__DIR__ . '/weblibs/', $newreleasepath. '/weblibs/'); //Copy required files
+
+	$counter = 0;
+	foreach ($PAGES as $PAGENAME => $PAGE) {
+		if ($counter == 0) {
+			fwrite(fopen($newreleasepath . 'index.html', "w"),twigrender($PAGE['TWIG']));
+		} else {
+			mkdir($newreleasepath . $PAGENAME,0777,true);
+			fwrite(fopen($newreleasepath . $PAGENAME . '/index.html', "w"),twigrender($PAGE['TWIG']));
+		}
+		$counter++;
+	}
+
+	echo "Completed - Please check " . $newreleasepath . " for your project\n\n";
 } else {
-	if (!isset($PAGES[reset(array_keys($_GET))])) $PAGE = $PAGES['INDEX'];
-	else $PAGE = $PAGES[reset(array_keys($_GET))];
+	$request = parse_url($_SERVER['REQUEST_URI']);
+	$path = $request["path"];
+
+	$result = trim(str_replace(basename($_SERVER['SCRIPT_NAME']), '', $path), '/');
+
+	$result = explode('/', $result);
+	$max_level = 2;
+	while ($max_level < count($result)) {
+		unset($result[0]);
+	}
+	$result = implode('/', $result);
+	echo $result;
+	//if (!isset($PAGES[reset(array_keys($_GET))])) $PAGE = $PAGES[0];
+	//else $PAGE = $PAGES[reset(array_keys($_GET))];
+
+	//die(twigrender($PAGE['TWIG']));
 }
 ?>
-
-BUNDLER
